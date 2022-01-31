f240a7eb51109998b7fb223b6c8f36ef56f82816
Mateus <mateusbarbozamarques99@gmail.com>
2022-01-30 21:11:03 -0300

    commit
    atualizaçoes

--- Dicionarios/Funcoes.py
+++ Dicionarios/Funcoes.py
@@ -1,11 +1,19 @@
-def perguntar () :
+def perguntar():
     return input("o que deseja realizar?\n" +
-                  "<I> - Para Iserir um usuário\n" +
-                  "<P> - Para Pesquisar um usuário\n" +
-                  "<E> - Para Excluir um usuário:\n" +
-                  "<L> - Para Listar um usuário: ").upper()
+                 "<I> - Para Iserir um usuário:\n" +
+                 "<P> - Para Pesquisar um usuário:\n" +
+                 "<E> - Para Excluir um usuário:\n" +
+                 "<L> - Para Listar um usuário: ").upper()
 
-def inserir (dicionario) :
+
+def inserir(dicionario):
     dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
-                                                  input("Digite a última data de acesso: "),
-                                                  input("Qual a última estação acessada: ").upper()]
+                                                     input("Digite a última data de acesso: "),
+                                                     input("Qual a última estação acessada: ").upper()]
+    salvar(dicionario)
+
+
+def salvar(dicionario):
+    with open("bd.txt", "a") as arquivo:
+        for chave, valor in dicionario.items():
+            arquivo.write(chave + ":" + str(valor))
